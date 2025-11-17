557ea17edc8d0dbb43a8569e49dc35b3e44301cf
Ann <anyakorytko@mail.ru>
2025-11-17 20:13:37 +0300

    feat: add reusable indicator

--- shared/ui/index.ts
+++ shared/ui/index.ts
@@ -1,3 +1,6 @@
 export { Button } from './button'
 export type { ButtonProps, ButtonVariant, ButtonSize, ButtonConfig } from './button'
 
+export { SkillIndicator } from './indicator'
+export type { SkillIndicatorProps } from './indicator'
+
